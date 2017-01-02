1dc874e7d5bb32f7333e9668997b1d123ea31efe
Ujjawal <11.ujjawal@gmail.com>
2017-01-02 22:13:17 +0530

    header background

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
+'use strict';
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cssmin = require('gulp-cssmin');
+const gls = require('gulp-live-server');
+
+gulp.task('server', () => {
+    let server = gls('server/bin/www', {
+        env: {
+            NODE_ENV: 'development'
+        }
+    }, 35729);
+
+    server.start();
+});
 
 gulp.task('styles', () => {
-    return gulp.src('client/sass/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('client/assets/css'))
+    return gulp.src('public/src/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/assets/css/'))
     .pipe(cssmin());
 });
 
-gulp.task('default', () => {
-    gulp.watch('client/sass/*.sass', ['styles']);
-})
+gulp.task('default', ['styles', 'server'], () => {
+    gulp.watch('public/src/scss/*.scss', ['styles']);
+});
